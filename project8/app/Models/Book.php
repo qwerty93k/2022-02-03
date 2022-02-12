c793558805256b88ce640e236bd62d660ab0f021
Mindaugas <qwerty93k@gmail.com>
2022-02-12 10:47:48 +0200

    sortable

--- project8/app/Models/Book.php
+++ project8/app/Models/Book.php
@@ -5,10 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Kyslik\ColumnSortable\Sortable;
 
 class Book extends Model
 {
     use HasFactory;
+
+    use Sortable;
+
+    public $sortable = ['id', 'title', 'description', 'author_id'];
 
     public function bookAuthor()
     {
